a9f57c1cd7762ec6a5db7d4d37e275d90da58ce9
trungmai666 <167017730+trungmai666@users.noreply.github.com>
2024-05-04 22:12:02 +0200

    Create i.js

---
+++ client/src/i.js
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import { EventListContext } from "./EventListContext.js";
+
+import Button from "react-bootstrap/esm/Button.js";
+
+import EventCard from "./EventCard";
+import EventForm from "./EventForm.js";
+import Container from "react-bootstrap/esm/Container.js";
+
+import Icon from "@mdi/react";
+import { mdiPlusBoxOutline, mdiPlusBoxMultipleOutline } from "@mdi/js";
+import ConfirmDeleteDialog from "./ConfirmDeleteDialog.js";
+
+function EventList() {
+  const { eventList } = useContext(EventListContext);
+  const [showEventForm, setShowEventForm] = useState(false);
+  const [showConfirmDeleteDialog, setShowConfirmDeleteDialog] = useState(false);
+
+  const filteredEventList = eventList.filter(
+    (event) => new Date(event.date) > new Date()
+  );
+
+  return (
+    <Container>
+      <div style={{ display: "flex", justifyContent: "flex-end", gap: "8px" }}>
+        <Button variant="success" onClick={() => setShowEventForm({})}>
+          <Icon path={mdiPlusBoxOutline} size={1} color={"white"} /> Nová
+          událost
+        </Button>
+        <Button variant="success" disabled>
+          <Icon path={mdiPlusBoxMultipleOutline} size={1} color={"white"} />{" "}
+          Nové události
+        </Button>
+      </div>
+      {!!showEventForm ? (
+        <EventForm event={showEventForm} setShowEventForm={setShowEventForm} />
+      ) : null}
+      {!!showConfirmDeleteDialog ? (
+        <ConfirmDeleteDialog
+          event={showConfirmDeleteDialog}
+          setShowConfirmDeleteDialog={setShowConfirmDeleteDialog}
+        />
+      ) : null}
+      {filteredEventList.map((event) => {
+        return (
+          <EventCard
+            key={event.id}
+            event={event}
+            setShowEventForm={setShowEventForm}
+            setShowConfirmDeleteDialog={setShowConfirmDeleteDialog}
+          />
+        );
+      })}
+    </Container>
+  );
+}
+
+export default EventList;
